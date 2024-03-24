e171a166e444c851a16fdd0baec9b2998f0b8e00
Ravi chandra <githubravi01@gmail.com>
2024-03-24 14:16:54 +0530

    User & Video Models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,55 +1,99 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
     username: {
-        type : String,
-        required: true,
-        unique: true,
-        lowercase : true,
-        trim: true,
-        index: true // Male element searchable
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // Male element searchable
     },
     email: {
-        type : String,
-        required: true,
-        unique: true,
-        lowercase : true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullname: {
-        type : String,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String, // Cloudnary service
-        required: true,
+      type: String, // Cloudnary service
+      required: true,
     },
     coverImage: {
-        type : String, // Cloudnary
+      type: String, // Cloudnary
     },
-    // Array 
+    // Array
     watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            reg: "Video"
-        }
+      {
+        type: Schema.Types.ObjectId,
+        reg: "Video",
+      },
     ],
     password: {
-        // Password is Encrypted 
-        type: String,
-        required: [true, "Password is Required"]
+      // Password is Encrypted
+      type: String,
+      required: [true, "Password is Required"],
     },
     refreshToken: {
-        type: String,
-    }
+      type: String,
+    },
     // Timestamps gives the CreatedAt and Updated At details
   },
   {
-    timestamp: true
+    timestamp: true,
   }
-)
+);
 
-export const User = mongoose.model("User",userSchema);
+// Encrypting the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+//  Designing Custom methods for Checking Password
+
+userSchema.methods.isPasswordCorrect = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Refresh Token
+userSchame.methods.generateRefreshToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
